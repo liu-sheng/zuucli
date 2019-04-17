6d1ce29ab21f1bf9b0854fa76d40bcfcfd601d36
liu-sheng <liusheng2048@gmail.com>
2019-04-17 15:51:21 +0800

    update

--- zuulcli/builds.py
+++ zuulcli/builds.py
@@ -20,6 +20,45 @@ class BuildsList(lister.Lister):
         parser.add_argument('--long',
                             help='list zuul builds info with more properties',
                             )
+        parser.add_argument('--project',
+                            help='list zuul builds info filtered by project',
+                            )
+        parser.add_argument('--pipeline',
+                            help='list zuul builds info filtered by pipeline',
+                            )
+        parser.add_argument('--change',
+                            help='list zuul builds info filtered by change',
+                            )
+        parser.add_argument('--branch',
+                            help='list zuul builds info filtered by branch',
+                            )
+        parser.add_argument('--patchset',
+                            help='list zuul builds info filtered by patchset',
+                            )
+        parser.add_argument('--ref',
+                            help='list zuul builds info filtered by ref',
+                            )
+        parser.add_argument('--newrev',
+                            help='list zuul builds info filtered by newrev',
+                            )
+        parser.add_argument('--uuid',
+                            help='list zuul builds info filtered by uuid',
+                            )
+        parser.add_argument('--job-name',
+                            help='list zuul builds info filtered by job name',
+                            )
+        parser.add_argument('--voting',
+                            help='list zuul builds info filtered by voting',
+                            )
+        parser.add_argument('--node-name',
+                            help='list zuul builds info filtered by node name',
+                            )
+        parser.add_argument('--result',
+                            help='list zuul builds info filtered by result',
+                            )
+        parser.add_argument('--limit',
+                            help='list zuul builds info with limt, default as 50',
+                            )
         return parser
 
     def take_action(self, parsed_args):
@@ -27,7 +66,16 @@ class BuildsList(lister.Lister):
             headers, properties = self.headers_long, self.properties_long
         else:
             headers, properties = self.headers, self.properties
-        resp = self.app.http_request('/builds')
+        url = '/builds'
+        filters = ''
+        for filter in ['project', 'pipeline', 'change', 'branch', 'patchset',
+                       'ref', 'newrev', 'uuid', 'job_name', 'voting', 'node_name',
+                       'result', 'limit']:
+            if getattr(parsed_args, filter):
+                filters += "%s=%s" % (filter, getattr(parsed_args, filter))
+        if filters:
+            url += "?%s" % filters
+        resp = self.app.http_request(url)
         values = [[b[p] for p in properties] for b in resp.json()]
         return headers, values
 
